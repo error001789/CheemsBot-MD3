3c071e407f8ea17715cd61a6e243aff3be17ec07
error001789 <106907103+error001789@users.noreply.github.com>
2022-10-15 22:10:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,5 @@
 //════════════════════════════//
-//If you want to recode, reupload
-//or copy the codes/script,
-//pls give credit
-//no credit? i will take action immediately
-//© 2022 Xeon Bot Inc. Cheems Bot MD
-//Thank you to Lord Buddha, Family and Myself
-//════════════════════════════//
-//recode kar ke youtube pe upload kar rhe ya
-//codes copy kar ke apne script me dal rhe
-//hai to, description me xeon ka yt channel
-// ka link paste kr dena as a cradit or github 
-//repo me bhi tag kardena baki jo
-//bhi karna hai apki marzi, thank you!🦄
-//════════════════════════════//
-//If you recode and uploading on your channel
-//or copy pasting the codes in ur script, 
-//i give permission to do as long as you
-//put Xeons youtube channel link in the video
-//description and tag me on githuh repo, 
-//thank you🦄
+
 //════════════════════════════//
 
 const fs = require("fs")
@@ -41,26 +22,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['94712870635'] //ur owner number
+global.ownername = "Radeeshana D"
+global.ytname = "YT: pacatama" //ur yt chanel name
+global.socialm = "GitHub: error001789" //ur github or insta name
+global.location = "Sri Lanka" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
+global.botname = "Queen Sara MD" //ur bot name
+global.websitex = "https://www.github.com/error001789" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
+global.packname = "Queen Sara Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
+global.author = "ERROR 001"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
-global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
+global.ownertag = ['94712870635'] //ur owner tag
+global.ownernummenu = ['94712870635'] //ur owner number in menu and all
+global.watermark = "🐉ＬＵＣ_🅧_ＳＥＣ🐉." //ur watermark
+global.botscript = "https://github.com/error001789"
+global.linkz1 = "http:wa.me/+94712870635"
+global.linkz2 = "http:wa.me/+94712870635"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
